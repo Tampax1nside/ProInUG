2c18e20c1abe5f97288c26ea4b2c7eb3c3c0b4f6
Tampax1nside <mac_1984@mail.ru>
2021-12-19 18:20:59 +0300

    создание компонента редактирования PaymentPoint

---
+++ ProInUG.BlazorUI/Components/PaymentPointEditComponent.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using ProInUG.BlazorUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProInUG.BlazorUI.Components
+{
+    public partial class PaymentPointEditComponent
+    {
+        [Parameter]
+        public EventCallback<PaymentPoint> OnSubmitCallback { get; set; }
+
+        [Parameter]
+        public EventCallback OnCancelCallback { get; set; }
+
+        [Parameter]
+        public PaymentPoint paymentPoint { get; set; } = new();
+
+        [Parameter]
+        public string FormName { get; set; } = "";
+
+        [Parameter]
+        public string SubmitButtonName { get; set; } = "Create";
+
+        [Parameter]
+        public bool IsSubmitButtonDisable { get; set; }
+    }
+}
