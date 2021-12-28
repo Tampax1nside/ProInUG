fe6cf2e0c7db7be91570c98b788c0e650dd07ab1
Tampax1nside <mac_1984@mail.ru>
2021-12-28 15:46:53 +0300

    Рефакторинг для читабельности кода

--- ProInUG.BlazorUI/Components/DialogDeletePaymentPoint.razor.cs
+++ ProInUG.BlazorUI/Components/DialogDeletePaymentPoint.razor.cs
@@ -10,24 +10,21 @@ namespace ProInUG.BlazorUI.Components
 {
     public partial class DialogDeletePaymentPoint
     {
-        [CascadingParameter]
-        MudDialogInstance? MudDialog { get; set; }
+        [CascadingParameter] private MudDialogInstance? MudDialog { get; set; }
 
         [Parameter]
-        public PaymentPoint paymentPoint { get; set; } = new();
+        public PaymentPoint PaymentPoint { get; set; } = new();
 
         private void Cancel()
         {
-            if (MudDialog != null)
-                MudDialog.Cancel();
+            MudDialog?.Cancel();
         }
 
         private void DeletePaymentPoint()
         {
             //In a real world scenario this bool would probably be a payment point to delete the item from api/database
             //Snackbar.Add("Server Deleted", Severity.Success);
-            if (MudDialog != null)
-                MudDialog.Close(DialogResult.Ok(paymentPoint.Id));
+            MudDialog?.Close(DialogResult.Ok(PaymentPoint.Id));
         }
     }
 }
