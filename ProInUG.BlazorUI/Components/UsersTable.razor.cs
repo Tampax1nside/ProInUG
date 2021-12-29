2cb26af8e341cd6ac350ec6a9301cfb6317e19b6
Tampax1nside <mac_1984@mail.ru>
2021-12-29 21:52:47 +0300

    Компонент - таблица пользователей

---
+++ ProInUG.BlazorUI/Components/UsersTable.razor.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using ProInUG.BlazorUI.Dto;
+using ProInUG.BlazorUI.Services;
+
+namespace ProInUG.BlazorUI.Components
+{
+    public partial class UsersTable
+    {
+        #region DI
+        [Inject] public IUsersService? UsersService { get; set; }
+        [Inject] public IDialogService? DialogService { get; set; }
+
+        #endregion
+        
+        private List<UserViewModel> _usersList = new();
+        private bool _loading;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetUsersListAsync();
+        }
+
+        private async Task GetUsersListAsync()
+        {
+            if (UsersService == null) return;
+            _loading = true;
+            
+            // TODO: убрать
+            await Task.Delay(700);
+            var (error, users) = await UsersService.GetAccounts();
+            _loading = false;
+            
+            // Success
+            if (error == 0 && users != null)
+            {
+                _usersList.Clear();
+                foreach (var user in users)
+                {
+                    _usersList.Add(new UserViewModel{User = user});
+                }
+            }
+            
+            // Error
+            if (error != 0)
+            {
+                var dialogMessage = ShowErrorDialog("Ошибка",$"Невозможно получить список пользователей <br />" +
+                                                             $"Код ошибки: {error.ToString()} <br />");
+                if (dialogMessage == null)
+                    return;
+                await dialogMessage.Result;
+            }
+        }
+        
+        // TODO: один и тот же код в двух местах - переделать
+        /// <summary>
+        /// Показать диалог с ошибкой
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private IDialogReference? ShowErrorDialog(string title, string message)
+        {
+            DialogOptions options = new() { CloseButton = true };
+            var parameters = new DialogParameters
+            {
+                {"ContentText", message},
+                {"ButtonText", "Закрыть"},
+                {"Color", Color.Error}
+            };
+            return DialogService?.Show<DialogLoginPage>(title, parameters, options);
+        }
+    }
+
+    public class UserViewModel
+    {
+        public bool ShowDetails { get; set; }
+        public AccountDto User { get; set; } = new();
+    }
+}
