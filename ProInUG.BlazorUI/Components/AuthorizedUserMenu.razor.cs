d9246c30f97aede5eaf078f593a208a73b677d1c
Tampax1nside <mac_1984@mail.ru>
2021-12-29 12:33:10 +0300

    Компонент верхнего меню пользователя

---
+++ ProInUG.BlazorUI/Components/AuthorizedUserMenu.razor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using ProInUG.BlazorUI.Services;
+
+namespace ProInUG.BlazorUI.Components
+{
+    public partial class AuthorizedUserMenu
+    {
+        [Inject] public AuthenticationStateProvider? AuthenticationStateProvider { get; set; }
+        
+        private IEnumerable<Claim> _claims = Enumerable.Empty<Claim>();
+
+        private ClaimsPrincipal? _user = new ClaimsPrincipal();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetClaimsPrincipalData();
+        }
+
+        private async Task GetClaimsPrincipalData()
+        {
+            if(AuthenticationStateProvider == null)
+                return;
+            
+            var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            _user = authState.User;
+            if (_user.Identity != null && _user.Identity.IsAuthenticated)
+            {
+                _claims = _user.Claims;
+            }
+        }
+        
+        private async Task LogoutAsync()
+        {
+            if (AuthenticationStateProvider == null)
+            {
+                return;
+            }
+
+            // TODO: убрать
+            await Task.Delay(2000);
+            var asp = (CwAuthenticationStateProvider)AuthenticationStateProvider;
+            await asp.LogoutAsync();
+        }
+    }
+}
