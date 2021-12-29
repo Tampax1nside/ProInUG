62fe58a9b682e54876cf230e9e8ac9ca297c1873
Tampax1nside <mac_1984@mail.ru>
2021-12-29 16:17:04 +0300

    Интерфейс работы с аккаунтами пользователей

---
+++ ProInUG.BlazorUI/Services/IUsersService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ProInUG.BlazorUI.Dto;
+
+namespace ProInUG.BlazorUI.Services
+{
+    public interface IUsersService
+    {
+        /// <summary>
+        /// Получить текущий аккаунт
+        /// </summary>
+        /// <returns></returns>
+        Task<(int Error, AccountDto Account)> GetCurrentAccount();
+
+        /// <summary>
+        /// Создать новый аккаунт
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
+        Task<(int Error, AccountDto Account)> CreateAccount(AccountDto account);
+        
+        /// <summary>
+        /// Изменить аккаунт
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
+        Task<(int Error, AccountDto Account)> UpdateAccount(AccountDto account);
+        
+        /// <summary>
+        /// Получить все аккаунты
+        /// </summary>
+        /// <returns></returns>
+        Task<(int Error, List<AccountDto> Account)> GetAccounts();
+        
+        /// <summary>
+        /// Удалить аккаунт
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        Task<int> DeleteAccount(string userId);
+
+        /// <summary>
+        /// Изменить пароль для аккаунта
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        Task<int> ChangeUserPassword(string userId, string password);
+    }
+}
