f888be0e648607e33f41407062487d63229dcafb
Tampax1nside <mac_1984@mail.ru>
2021-12-30 00:01:19 +0300

    Диалоговое окно создания пользователя

---
+++ ProInUG.BlazorUI/Components/DialogCreateUser.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using ProInUG.BlazorUI.Dto;
+
+namespace ProInUG.BlazorUI.Components
+{
+    public partial class DialogCreateUser
+    {
+        [CascadingParameter] private MudDialogInstance? MudDialog { get; set; }
+
+        [Parameter] public CreateAccountDto User { get; set; } = new();
+        
+        [Parameter] public string SubmitButtonName { get; set; } = "Create";
+
+        [Parameter] public string TextContent { get; set; } = "";
+        
+        private void Cancel() => MudDialog?.Cancel();
+
+        private void Submit() => MudDialog?.Close(DialogResult.Ok(User));
+    }
+}
