55ac7bee9d14452ef4bb19d23c6d0e0a043b2946
Tampax1nside <mac_1984@mail.ru>
2021-12-30 00:01:01 +0300

    DTO для создания аккаунта

---
+++ ProInUG.BlazorUI/Dto/CreateAccountDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProInUG.BlazorUI.Dto
+{
+    public class CreateAccountDto
+    {
+        [StringLength(256, MinimumLength = 8)] public string Name { get; set; } = "";
+
+        public Roles Role { get; set; }
+
+        public string Description { get; set; } = "";
+
+        [StringLength(256, MinimumLength = 8)] public string Password { get; set; } = "";
+    }
+}
