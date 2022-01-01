7236b359f0daf11a73d51a3c8732b45f10a3a63d
Tampax1nside <mac_1984@mail.ru>
2022-01-01 17:00:22 +0300

    Методы удаления пользователя и изменения пароля пользователя

--- ProInUG.BlazorUI/Components/UsersTable.razor.cs
+++ ProInUG.BlazorUI/Components/UsersTable.razor.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
 using MudBlazor;
 using ProInUG.BlazorUI.Dto;
 using ProInUG.BlazorUI.Services;
@@ -14,6 +16,8 @@ namespace ProInUG.BlazorUI.Components
         #region DI
         [Inject] public IUsersService? UsersService { get; set; }
         [Inject] public IDialogService? DialogService { get; set; }
+
+        [Inject] public ILogger<UsersTable>? Logger { get; set; }
 
         #endregion
         
@@ -67,6 +71,118 @@ namespace ProInUG.BlazorUI.Components
         }
 
         /// <summary>
+        /// Изменить пароль пользователя
+        /// </summary>
+        /// <param name="user"></param>
+        private async Task ChangePasswordAsync(AccountDto user)
+        {
+            if (UsersService == null)
+                return;
+
+            var userViewModel = new ChangePasswordViewModel {User = user}; 
+            var dialogWindow = ShowChangePasswordDialog(userViewModel);
+            
+            if(dialogWindow == null) return;
+            
+            var result = await dialogWindow.Result;
+
+            if (!result.Cancelled && result.Data != null)
+            {
+                try
+                {
+                    userViewModel = (ChangePasswordViewModel) result.Data;
+                    
+                    var dialogProcess = ShowProcessDialog("processing ...");
+                    // TODO: удалить
+                    await Task.Delay(1000);
+
+                    dialogProcess?.Close();
+
+                    IDialogReference? dialogMessage;
+                    var error = await UsersService.ChangeUserPassword(userViewModel.User.Id.ToString(), userViewModel.Password);
+                    
+                    if (error != 0)
+                    {
+                        dialogMessage = ShowErrorDialog("Ошибка","Невозможно изменить пароль пользователя <br />" +
+                                                                 $"Код ошибки {error}");
+                        if (dialogMessage == null)
+                            return;
+                        await dialogMessage.Result;
+                        return;
+                    }
+                    
+                    dialogMessage = ShowSuccessDialog("Изменение пароля",$"Пароль пользователя {user.Name} успешно изменен.");
+                    if (dialogMessage == null)
+                        return;
+                    await dialogMessage.Result;
+                    
+                }
+                catch (InvalidCastException castEx)
+                {
+                    Logger.LogCritical(castEx,
+                        "Невозможно привести  результат диалогового окна в тип ChangePasswordViewModel.");
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogCritical(ex, "Ошибка изменения пароля пользователя.");
+                }
+                
+            }
+        }
+
+        /// <summary>
+        /// Удалить пользователя
+        /// </summary>
+        /// <param name="user"></param>
+        private async Task DeleteUserAsync(AccountDto user)
+        {
+            if (UsersService == null)
+                return;
+
+            var dialogWindow = ShowDeleteDialog(user);
+            
+            if (dialogWindow == null)
+                return;
+            
+            var result = await dialogWindow.Result;
+
+            if (!result.Cancelled && result.Data != null)
+            {
+                var id = result.Data.ToString();
+                if (id != null)
+                {
+                    var dialogProcess = ShowProcessDialog("processing ...");
+                    // TODO: удалить
+                    await Task.Delay(1000);
+
+                    var error = await UsersService.DeleteAccount(id.ToString());
+                    
+                    dialogProcess?.Close();
+
+                    IDialogReference? dialogMessage;
+                    
+                    if (error != 0)
+                    {
+                        dialogMessage = ShowErrorDialog("Ошибка","Невозможно удалить пользователя <br />" +
+                                                                 $"Код ошибки {error}");
+                        if (dialogMessage == null)
+                            return;
+                        await dialogMessage.Result;
+                        return;
+                    }
+
+                    dialogMessage = ShowSuccessDialog("Удаление пользователя",$"Пользователь {user.Name} успешно удален");
+                    if (dialogMessage == null)
+                        return;
+                    await dialogMessage.Result;
+                    
+                    _itemsChanged = true;
+                    await GetUsersListAsync();   
+                }
+            }
+        }
+
+        /// <summary>
         /// Редактировать пользователя
         /// </summary>
         /// <param name="user"></param>
@@ -80,6 +196,34 @@ namespace ProInUG.BlazorUI.Components
             var result = await editDialog.Result;
             if (result.Cancelled) return;
             
+            var dialogProcess = ShowProcessDialog("processing ...");
+            
+            // TODO: удалить
+            await Task.Delay(1000);
+
+            var (error, account) = await UsersService.UpdateAccount((AccountDto) result.Data);
+            
+            dialogProcess?.Close();
+
+            IDialogReference? dialogMessage;
+            
+            if (error != 0 || account == null)
+            {
+                dialogMessage = ShowErrorDialog("Ошибка","Невозможно изменить данного пользователя <br />" +
+                                                         $"Код ошибки {error}");
+                if (dialogMessage == null)
+                    return;
+                await dialogMessage.Result;
+                return;
+            }
+
+            dialogMessage = ShowSuccessDialog("Редактирование пользователя","Пользователь успешно изменен");
+            if (dialogMessage == null)
+                return;
+            await dialogMessage.Result;
+
+            _itemsChanged = true;
+            await GetUsersListAsync();
         }
         
         /// <summary>
@@ -116,7 +260,7 @@ namespace ProInUG.BlazorUI.Components
                 return;
             }
 
-            dialogMessage = ShowSuccessDialog("Новый пользователь успешно создан.");
+            dialogMessage = ShowSuccessDialog("Создание пользователя","Новый пользователь успешно создан.");
             if (dialogMessage == null)
                 return;
             await dialogMessage.Result;
@@ -124,13 +268,14 @@ namespace ProInUG.BlazorUI.Components
             _itemsChanged = true;
             await GetUsersListAsync();
         }
-        
+
         /// <summary>
         /// Показать диалог - успешное выполнение
         /// </summary>
+        /// <param name="title"></param>
         /// <param name="message"></param>
         /// <returns></returns>
-        private IDialogReference? ShowSuccessDialog(string message)
+        private IDialogReference? ShowSuccessDialog(string title, string message)
         {
             DialogOptions options = new() { CloseButton = true, MaxWidth = MaxWidth.ExtraSmall, FullWidth = true};
             var parameters = new DialogParameters
@@ -139,7 +284,7 @@ namespace ProInUG.BlazorUI.Components
                 {"ButtonText", "Close"},
                 {"Color", Color.Default}
             };
-            return DialogService?.Show<DialogLoginPage>("Создание пользователя", parameters, options);
+            return DialogService?.Show<DialogLoginPage>(title, parameters, options);
         }
         
         /// <summary>
@@ -151,6 +296,18 @@ namespace ProInUG.BlazorUI.Components
             DialogOptions options = new() { MaxWidth = MaxWidth.ExtraSmall, FullWidth = true };
             var parameters = new DialogParameters { ["SubmitButtonName"] = "Создать"};
             return DialogService?.Show<DialogCreateUser>("Создать нового пользователя", parameters, options);
+        }
+        
+        /// <summary>
+        /// Диалоговое окно удаления пользователя
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        private IDialogReference? ShowDeleteDialog(AccountDto user)
+        {
+            DialogOptions options = new DialogOptions() { MaxWidth = MaxWidth.ExtraSmall, FullWidth = true };
+            var parameters = new DialogParameters { ["User"] = user };
+            return DialogService?.Show<DialogDeleteUser>("Delete User", parameters, options);
         }
         
         /// <summary>
@@ -183,6 +340,22 @@ namespace ProInUG.BlazorUI.Components
             };
             return DialogService?.Show<DialogLoginPage>(title, parameters, options);
         }
+
+        /// <summary>
+        /// Показать диалог изменения пароля
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        private IDialogReference? ShowChangePasswordDialog(ChangePasswordViewModel user)
+        {
+            DialogOptions options = new() { MaxWidth = MaxWidth.ExtraSmall, FullWidth = true };
+            var parameters = new DialogParameters
+            {
+                ["SubmitButtonName"] = "Изменить пароль",
+                ["ChangePasswordViewModel"] = user
+            };
+            return DialogService?.Show<DialogChangeUserPassword>("Изменить пароль", parameters, options);
+        }
         
         /// <summary>
         /// Показать Processing диалог
@@ -210,6 +383,14 @@ namespace ProInUG.BlazorUI.Components
     public class UserViewModel
     {
         public bool ShowDetails { get; set; }
+        public AccountDto User { get; set; } = new();
+    }
+
+    public class ChangePasswordViewModel
+    {
+        [StringLength(256, MinimumLength = 8)]
+        public string Password { get; set; } = "";
+        
         public AccountDto User { get; set; } = new();
     }
 }
