621e79dbc17b67ae6a6d6f6427febfc5d4d722c9
Tampax1nside <mac_1984@mail.ru>
2022-01-01 16:56:45 +0300

    Изменение пароля пользователя

---
+++ ProInUG.BlazorUI/Components/DialogChangeUserPassword.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace ProInUG.BlazorUI.Components
+{
+    public partial class DialogChangeUserPassword
+    {
+        [CascadingParameter] private MudDialogInstance? MudDialog { get; set; }
+
+        [Parameter] public ChangePasswordViewModel ChangePasswordViewModel { get; set; } = new();
+        
+        [Parameter] public string SubmitButtonName { get; set; } = "Change password";
+
+        [Parameter] public string TextContent { get; set; } = "";
+        
+        private void Cancel() => MudDialog?.Cancel();
+
+        private void Submit() => MudDialog?.Close(DialogResult.Ok(ChangePasswordViewModel));
+    }
+}
