9d5ac73e5444926cba626cea678b86d6b69b8bcb
Tampax1nside <mac_1984@mail.ru>
2022-01-01 16:57:48 +0300

    Диалог удаления пользователя

---
+++ ProInUG.BlazorUI/Components/DialogDeleteUser.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using ProInUG.BlazorUI.Dto;
+using ProInUG.BlazorUI.Models;
+
+namespace ProInUG.BlazorUI.Components
+{
+    public partial class DialogDeleteUser
+    {
+        [CascadingParameter] private MudDialogInstance? MudDialog { get; set; }
+
+        [Parameter]
+        public AccountDto User { get; set; } = new();
+
+        private void Cancel()
+        {
+            MudDialog?.Cancel();
+        }
+
+        private void DeleteUser()
+        {
+            MudDialog?.Close(DialogResult.Ok(User.Id));
+        }
+    }
+}
